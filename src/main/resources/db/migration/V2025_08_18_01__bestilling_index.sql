8625d7a2738b38d4963341658eb5f22d6a00d387
Lars Hartvigsen <lars.hartvigsen@nav.no>
2025-08-19 08:07:26 +0200

    Endrer navn på tabell til bare Bestilling. Fordi det er ikke noen andre slags bestillinger her

---
+++ src/main/resources/db/migration/V2025_08_18_01__bestilling_index.sql
@@ -0,0 +1,3 @@
+SET lock_timeout = '5s';
+
+create index IX_FK_BESTILLING_ID on BESTILLING (ID)
--- src/test/resources/basicendtoendtest/basicdata.sql
+++ src/test/resources/basicendtoendtest/basicdata.sql
@@ -1,2 +1,3 @@
 INSERT INTO aktoer (id, navn, skjermet, ansatt) values (7917506, 'Signe Maten', false, false);
 INSERT INTO aktoer_offnr (id, aktoer_id, lopenr, gjelder_fom, aktoer_ident) VALUES (1, 7917506, 9999, '2025-05-09', '12466711096');
+INSERT INTO BESTILLING (FNR, INNTEKTSAAR) VALUES ('12345678901', '2025');

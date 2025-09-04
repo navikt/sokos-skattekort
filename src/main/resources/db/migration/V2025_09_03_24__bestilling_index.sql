e3b2664b0541590364729a42c57beeb5dfe077a3
geirh-nav <geir.harris.hedemark@nav.no>
2025-09-04 08:07:07 +0200

    TOB-5290: Relasjon mellom bestillinger og bestillinger_batch gikk feil vei

--- src/main/resources/db/migration/V2025_09_03_24__bestilling_index.sql
+++ src/main/resources/db/migration/V2025_09_03_24__bestilling_index.sql
@@ -1,4 +1,4 @@
 SET lock_timeout = '5s';
 
 CREATE INDEX IF NOT EXISTS bestillinger_aktoer_id ON bestillinger (aktoer_id);
-CREATE INDEX IF NOT EXISTS bestillinger_batch_bestilling_id ON bestillinger_batch (bestilling_id);
+CREATE INDEX IF NOT EXISTS bestillinger_bestillinger_batch_id ON bestillinger (bestilling_batch_id);

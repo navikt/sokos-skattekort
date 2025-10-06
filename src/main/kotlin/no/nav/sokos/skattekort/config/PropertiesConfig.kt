bb66deb499b9bfde4e3bfc810ddb03d6cb4158d3
takwaiwang <tak.wai.wang@nav.no>
2025-10-06 14:49:35 +0200

    TOB-5318 legg til DB og MQ secret

--- src/main/kotlin/no/nav/sokos/skattekort/config/PropertiesConfig.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/config/PropertiesConfig.kt
@@ -48,6 +48,7 @@ object PropertiesConfig {
             naisAppName = getOrEmpty("NAIS_APP_NAME"),
             environment = Environment.valueOf(getOrEmpty("ENVIRONMENT")),
             useAuthentication = getOrEmpty("USE_AUTHENTICATION").toBoolean(),
+            mqListenerEnabled = getOrEmpty("MQ_LISTENER_ENABLED").toBoolean(),
         )
 
     fun getPostgresProperties(): PostgresProperties =
@@ -87,6 +88,7 @@ object PropertiesConfig {
         val naisAppName: String,
         val environment: Environment,
         val useAuthentication: Boolean,
+        val mqListenerEnabled: Boolean,
     )
 
     data class PostgresProperties(

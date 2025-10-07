86677dfb3aec78556ab2ca70b2fa3a8049f48b67
takwaiwang <tak.wai.wang@nav.no>
2025-10-07 10:26:46 +0200

    oppdater setupLocalEnvironment og setter MQPropreties.userAuth=true

--- setupLocalEnvironment.sh
+++ setupLocalEnvironment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export VAULT_ADDR=https://vault.adeo.no
 
 # Ensure user is authenicated, and run login if not.
 gcloud auth print-identity-token &> /dev/null
@@ -8,10 +9,26 @@ fi
 kubectl config use-context dev-fss
 kubectl config set-context --current --namespace=okonomi
 
+# Get database username and password secret from Vault
+[[ "$(vault token lookup -format=json | jq '.data.display_name' -r; exit ${PIPESTATUS[0]})" =~ "nav.no" ]] &>/dev/null || vault login -method=oidc -no-print
+
 # Get AZURE system variables
 envValue=$(kubectl exec -it $(kubectl get pods | grep sokos-skattekort | cut -f1 -d' ') -c sokos-skattekort -- env | egrep "^AZURE|^MASKINPORTEN|^MQ_SERVICE"| sort)
+
+POSTGRES_USER=$(vault kv get -field=data postgresql/preprod-fss/creds/sokos-skattekort-user)
+POSTGRES_ADMIN=$(vault kv get -field=data postgresql/preprod-fss/creds/sokos-skattekort-admin)
 
 # Set AZURE as local environment variables
 rm -f defaults.properties
 echo "$envValue" > defaults.properties
 echo "AZURE stores as defaults.properties"
+
+username=$(echo "$POSTGRES_USER" | awk -F 'username:' '{print $2}' | awk '{print $1}' | sed 's/]$//')
+password=$(echo "$POSTGRES_USER" | awk -F 'password:' '{print $2}' | awk '{print $1}' | sed 's/]$//')
+echo "POSTGRES_USER_USERNAME=$username" >> defaults.properties
+echo "POSTGRES_USER_PASSWORD=$password" >> defaults.properties
+
+username=$(echo "$POSTGRES_ADMIN" | awk -F 'username:' '{print $2}' | awk '{print $1}' | sed 's/]$//')
+password=$(echo "$POSTGRES_ADMIN" | awk -F 'password:' '{print $2}' | awk '{print $1}' | sed 's/]$//')
+echo "POSTGRES_ADMIN_USERNAME=$username" >> defaults.properties
+echo "POSTGRES_ADMIN_PASSWORD=$password" >> defaults.properties

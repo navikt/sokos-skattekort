3ff0fdbc4278fd5d50d714ba9f29655ce2353223
takwaiwang <tak.wai.wang@nav.no>
2025-10-10 11:28:01 +0200

    TOB-5284 opprett api støtte for maskinporten token

--- src/main/kotlin/no/nav/sokos/skattekort/config/RoutingConfig.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/config/RoutingConfig.kt
@@ -2,8 +2,12 @@ package no.nav.sokos.skattekort.config
 
 import io.ktor.server.application.Application
 import io.ktor.server.auth.authenticate
+import io.ktor.server.plugins.di.dependencies
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.routing
+
+import no.nav.sokos.skattekort.api.maksinportenTokenApi
+import no.nav.sokos.skattekort.security.MaskinportenTokenClient
 
 fun Application.routingConfig(
     useAuthentication: Boolean,
@@ -11,8 +15,10 @@ fun Application.routingConfig(
 ) {
     routing {
         internalNaisRoutes(applicationState)
-//        authenticate(useAuthentication, AUTHENTICATION_NAME) {
-//        }
+        authenticate(useAuthentication, AUTHENTICATION_NAME) {
+            val maskinportenTokenClient: MaskinportenTokenClient by dependencies
+            maksinportenTokenApi(maskinportenTokenClient)
+        }
     }
 }
 

b1d95ba4161bfb0f22de56faf28fb8b20d30596b
takwaiwang <tak.wai.wang@nav.no>
2025-10-13 10:48:37 +0200

    TOB-5536 legg til sekundær konstruktor i Utsending

--- src/main/kotlin/no/nav/sokos/skattekort/domain/utsending/Utsending.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/domain/utsending/Utsending.kt
@@ -3,7 +3,10 @@ package no.nav.sokos.skattekort.domain.utsending
 import kotlin.time.Clock
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
+import kotlin.time.toKotlinInstant
 import kotlinx.serialization.Serializable
+
+import kotliquery.Row
 
 import no.nav.sokos.skattekort.domain.forespoersel.AbonnementId
 import no.nav.sokos.skattekort.domain.forespoersel.Forsystem
@@ -18,7 +21,17 @@ data class Utsending
         val inntektsaar: Int,
         val forsystem: Forsystem,
         val opprettet: Instant = Clock.System.now(),
-    )
+    ) {
+        @OptIn(ExperimentalTime::class)
+        constructor(row: Row) : this(
+            id = row.long("id")?.let { UtsendingId(it) },
+            abonnementId = AbonnementId(row.long("abonnement_id")),
+            fnr = Personidentifikator(row.string("fnr")),
+            inntektsaar = row.int("inntektsaar"),
+            forsystem = Forsystem.fromValue(row.string("forsystem")),
+            opprettet = row.instant("opprettet").toKotlinInstant(),
+        )
+    }
 
 @Serializable
 @JvmInline

cddda7aba6a146a06203b910d28cd9262c32a9a3
takwaiwang <tak.wai.wang@nav.no>
2025-10-13 08:48:05 +0200

    TOB-5282 lag utsendelsestabell

---
+++ src/main/kotlin/no/nav/sokos/skattekort/domain/utsending/UtsendingRepository.kt
@@ -0,0 +1,80 @@
+package no.nav.sokos.skattekort.domain.utsending
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.toKotlinInstant
+
+import kotliquery.Row
+import kotliquery.Session
+import kotliquery.TransactionalSession
+import kotliquery.queryOf
+
+import no.nav.sokos.skattekort.domain.forespoersel.AbonnementId
+import no.nav.sokos.skattekort.domain.forespoersel.Forsystem
+import no.nav.sokos.skattekort.domain.person.Personidentifikator
+
+object UtsendingRepository {
+    fun insertBatch(
+        tx: TransactionalSession,
+        utsendingList: List<Utsending>,
+    ): List<Int> =
+        tx
+            .batchPreparedStatement(
+                """
+                INSERT INTO utsendinger (abonnement_id, fnr, inntektsaar, forsystem)
+                VALUES (?, ?, ?, ?)
+                """.trimIndent(),
+                utsendingList.map {
+                    listOf(
+                        it.abonnementId.value,
+                        it.fnr.value,
+                        it.inntektsaar,
+                        it.forsystem.kode,
+                    )
+                },
+            ).orEmpty()
+
+    fun delete(
+        tx: TransactionalSession,
+        id: UtsendingId,
+    ) {
+        tx.execute(
+            queryOf(
+                """
+                DELETE FROM utsendinger WHERE id = ?
+                """.trimIndent(),
+                id.value,
+            ),
+        )
+    }
+
+    fun getAllUtsendinger(session: Session): List<Utsending> {
+        val sql =
+            StringBuilder(
+                """
+                SELECT id, abonnement_id, fnr, inntektsaar, forsystem, opprettet
+                FROM utsendinger
+                """.trimIndent(),
+            )
+        return session.list(
+            queryOf(
+                """
+                SELECT id, abonnement_id, fnr, inntektsaar, forsystem, opprettet
+                FROM utsendinger
+                """.trimIndent(),
+            ),
+            extractor = mapToUtsending,
+        )
+    }
+
+    @OptIn(ExperimentalTime::class)
+    private val mapToUtsending: (Row) -> Utsending = { row ->
+        Utsending(
+            id = row.long("id")?.let { UtsendingId(it) },
+            abonnementId = AbonnementId(row.long("abonnement_id")),
+            fnr = Personidentifikator(row.string("fnr")),
+            inntektsaar = row.int("inntektsaar"),
+            forsystem = Forsystem.fromValue(row.string("forsystem")),
+            opprettet = row.instant("opprettet").toKotlinInstant(),
+        )
+    }
+}

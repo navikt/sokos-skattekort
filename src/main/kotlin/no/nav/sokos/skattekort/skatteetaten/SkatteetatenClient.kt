ba38d0df4922d4e5d6cd9a2196a872b41eba8b30
Lars Hartvigsen <lars.hartvigsen@nav.no>
2025-10-17 10:14:22 +0200

    TOB-5261 bruker eksisterende httpClient

--- src/main/kotlin/no/nav/sokos/skattekort/skatteetaten/SkatteetatenClient.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/skatteetaten/SkatteetatenClient.kt
@@ -2,36 +2,33 @@ package no.nav.sokos.skattekort.skatteetaten
 
 import kotlinx.serialization.json.Json
 
+import io.ktor.client.call.body
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.contentType
+
+import no.nav.sokos.skattekort.config.httpClient
 import no.nav.sokos.skattekort.security.MaskinportenTokenClient
 
 class SkatteetatenClient(
     private val maskinportenTokenClient: MaskinportenTokenClient,
 ) {
     suspend fun bestillSkattekort(request: SkatteetatenBestillSkattekortRequest): SkatteetatenBestillSkattekortResponse {
+        // Flyttes til nais-config når vi skal ha forskjellige miljøer
         val url = "https://api-test.sits.no/api/forskudd/bestillSkattekort/"
-        val client =
-            java.net.http.HttpClient
-                .newHttpClient()
 
         val requestBody = Json.encodeToString(request)
 
-        val request =
-            java.net.http.HttpRequest
-                .newBuilder()
-                .uri(java.net.URI.create(url))
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer ${maskinportenTokenClient.getAccessToken()}")
-                .POST(
-                    java.net.http.HttpRequest.BodyPublishers
-                        .ofString(requestBody),
-                ).build()
         val response =
-            client.send(
-                request,
-                java.net.http.HttpResponse.BodyHandlers
-                    .ofString(),
-            )
+            httpClient.post(url) {
+                contentType(ContentType.Application.Json)
+                header(HttpHeaders.Authorization, "Bearer ${maskinportenTokenClient.getAccessToken()}")
+                setBody(requestBody)
+            }
 
-        return Json.decodeFromString<SkatteetatenBestillSkattekortResponse>(response.body())
+        return response.body<SkatteetatenBestillSkattekortResponse>()
     }
 }

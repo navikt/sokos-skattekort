58bd9c2f1461e020a1c73b88ef55b22c6e7eaab4
Lars Hartvigsen <lars.hartvigsen@nav.no>
2025-10-21 09:43:13 +0200

    Endringer etter qa

--- src/main/kotlin/no/nav/sokos/skattekort/api/BestillingApi.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/api/BestillingApi.kt
@@ -7,7 +7,6 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import mu.KotlinLogging
 
-import no.nav.sokos.skattekort.config.TEAM_LOGS_MARKER
 import no.nav.sokos.skattekort.module.skattekort.BestillingsService
 
 private val logger = KotlinLogging.logger { }
@@ -15,7 +14,7 @@ private val logger = KotlinLogging.logger { }
 fun Route.bestillingApi(service: BestillingsService) {
     route("api/v1") {
         get("sendBestilling") {
-            logger.info(marker = TEAM_LOGS_MARKER) { "foerespoerselApi - Skal sende bestillingsbatch" }
+            logger.info { "foerespoerselApi - Skal sende bestillingsbatch" }
             service.opprettBestillingsbatch()
             call.respond(HttpStatusCode.OK)
         }

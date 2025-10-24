fc0ce7a516827fe10578398c82a98ffefedd7224
geirh-nav <geir.harris.hedemark@nav.no>
2025-10-24 12:03:19 +0200

    TOB-5264: Enkle tester for utsending til oppdrag z

--- src/test/kotlin/no/nav/sokos/skattekort/module/utsending/oppdragz/UtsendingCronJobTest.kt
+++ src/test/kotlin/no/nav/sokos/skattekort/module/utsending/oppdragz/UtsendingCronJobTest.kt
@@ -1,12 +1,16 @@
 package no.nav.sokos.skattekort.module.utsending.oppdragz
 
+import kotlin.test.assertTrue
+
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.extensions.testcontainers.toDataSource
 
 import no.nav.sokos.skattekort.JmsTestUtil
 import no.nav.sokos.skattekort.listener.DbListener
 import no.nav.sokos.skattekort.listener.MQListener
 import no.nav.sokos.skattekort.module.person.Audit
 import no.nav.sokos.skattekort.module.person.AuditService
+import no.nav.sokos.skattekort.module.person.AuditTag
 import no.nav.sokos.skattekort.module.person.PersonId
 import no.nav.sokos.skattekort.module.utsending.UtsendingService
 
@@ -27,10 +31,27 @@ class UtsendingCronJobTest :
                 uut.handleUtsending()
                 val auditEntries: List<Audit> = auditService.getAuditByPersonId(PersonId(3))
                 println(auditEntries)
-                assert(auditEntries.map { it.informasjon ?: "" }.any { it.contains("Skattekort sendt") })
+                assertTrue(auditEntries.map { it.tag }.contains(AuditTag.UTSENDING_OK))
                 val messages = JmsTestUtil.getMessages(MQListener.utsendingsQueue)
-                assert(messages.size == 1)
-                assert(messages.first().contains("12345678903"))
+                assertTrue(messages.size == 1)
+                assertTrue(messages.first().contains("12345678903"))
+            }
+            test("Vi skal håndtere feil i utsendelse til oppdragz") {
+                DbListener.loadDataSet("database/skattekort/person_med_skattekort.sql")
+                DbListener.loadDataSet("database/utsending/skattekort_oppdragz.sql")
+                DbListener.container.toDataSource().connection.use { connection ->
+                    connection.createStatement().use { statement ->
+                        statement.execute("UPDATE forskuddstrekk SET trekk_kode='foobar' WHERE id=5") // Vil ikke eksistere i Trekkode-enumen
+                    }
+                }
+                uut.handleUtsending()
+                val auditEntries: List<Audit> = auditService.getAuditByPersonId(PersonId(3))
+                println(auditEntries)
+                println(auditEntries.map { it.tag })
+                assertTrue(auditEntries.map { it.tag }.contains(AuditTag.UTSENDING_FEILET))
+                val messages = JmsTestUtil.getMessages(MQListener.utsendingsQueue)
+                assertTrue(messages.size == 0)
+                uut.
             }
         },
     )

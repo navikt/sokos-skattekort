9878b6a344491a61ca9bc7655829b98200e475ae
geirh-nav <geir.harris.hedemark@nav.no>
2025-10-30 19:39:04 +0100

    TOB-5264: Fiks for monitorering etter lokal kjøring

--- src/main/kotlin/no/nav/sokos/skattekort/metrics/Metrics.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/metrics/Metrics.kt
@@ -2,24 +2,9 @@ package no.nav.sokos.skattekort.metrics
 
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
-import io.prometheus.metrics.core.metrics.Counter
 
-private const val METRICS_NAMESPACE = "sokos_skattekort"
-
-private const val EXAMPLE_COUNTER = "${METRICS_NAMESPACE}_example_counter"
+const val METRICS_NAMESPACE = "sokos_skattekort"
 
 object Metrics {
     val prometheusMeterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
-
-    /**
-     * This is an example counter. It is used to demonstrate how to create a counter metric.
-     * To use this counter metric, you can call `exampleCounter.inc()` to increment the counter by 1.
-     */
-    val exampleCounter: Counter =
-        Counter
-            .builder()
-            .name(EXAMPLE_COUNTER)
-            .help("Example counter")
-            .withoutExemplars()
-            .register(prometheusMeterRegistry.prometheusRegistry)
 }

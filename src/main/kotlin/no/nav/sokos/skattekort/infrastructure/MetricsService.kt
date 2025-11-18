3ffd6cc0efa9514100f8a15cf500e91ebcb0d2da
geirh-nav <geir.harris.hedemark@nav.no>
2025-11-18 12:11:00 +0100

    TOB-5680: Fiks i dependencies så tester kjører

--- src/main/kotlin/no/nav/sokos/skattekort/infrastructure/MetricsService.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/infrastructure/MetricsService.kt
@@ -17,26 +17,23 @@ import no.nav.sokos.skattekort.util.SQLUtils.transaction
 
 class MetricsService(
     private val dataSource: DataSource,
-    private val skattekortRepository: SkattekortRepository,
-    private val bestillingRepository: BestillingRepository,
-    private val utsendingRepository: UtsendingRepository,
 ) {
     fun fetchMetrics() {
         dataSource.transaction { tx ->
             // Varsel om vi ikke har fått skattekort på 24 timer
             val naa = LocalDateTime.now()
-            val timeSisteSkattekortStored: LocalDateTime = skattekortRepository.getLatestSkattekortUpdateTime(tx) ?: naa
+            val timeSisteSkattekortStored: LocalDateTime = SkattekortRepository.getLatestSkattekortUpdateTime(tx) ?: naa
             timerSidenSisteSkattekortLMetric.set(Duration.between(timeSisteSkattekortStored, naa).toMinutes().div(60.0))
             // Bestillinger som ikke er løst i løpet av 30 minutter
-            val earliestBestillingTime: LocalDateTime = bestillingRepository.getEarliestBestillingTime(tx) ?: naa
+            val earliestBestillingTime: LocalDateTime = BestillingRepository.getEarliestBestillingTime(tx) ?: naa
             minutterSidenBestillingMetric.labelValues("eldste_lagret").set(Duration.between(earliestBestillingTime, naa).toSeconds().div(60.0))
-            val earliestSentBestillingTime = bestillingRepository.getEarliestSentBestillingTime(tx) ?: naa
+            val earliestSentBestillingTime = BestillingRepository.getEarliestSentBestillingTime(tx) ?: naa
             minutterSidenBestillingMetric.labelValues("eldste_sendt").set(Duration.between(earliestSentBestillingTime, naa).toSeconds().div(60.0))
             // Utsendinger som ikke er sendt i løpet av 5 minutter
-            val earliestUnsentUtsendingTime = utsendingRepository.getEarliestUnsentUtsendingTime(tx) ?: naa
+            val earliestUnsentUtsendingTime = UtsendingRepository.getEarliestUnsentUtsendingTime(tx) ?: naa
             minutterSidenUtsendingMetric.set(Duration.between(earliestUnsentUtsendingTime, naa).toSeconds().div(60.0))
 //            Totalt antall innhentede skattekort
-            val totalSkattekortCount: Map<ResultatForSkattekort, Int> = skattekortRepository.numberOfSkattekortByResultatForSkattekortMetrics(tx)
+            val totalSkattekortCount: Map<ResultatForSkattekort, Int> = SkattekortRepository.numberOfSkattekortByResultatForSkattekortMetrics(tx)
             totalSkattekortCount.map { (resultat, count) ->
                 totalSkattekortCountMetric.labelValues(resultat.value).set(count.toDouble())
             }
@@ -44,19 +41,19 @@ class MetricsService(
 //            Skattekort tabell på trekkodene Lønn fra Nav
 //            Skattekort tabell på trekkodene pensjon fra Nav
 //            Skattekort tabell på trekkodene uføretrygd fra Nav
-            val numberOfTabelltrekkByTrekkode: Map<Trekkode, Int> = skattekortRepository.numberOfForskuddstrekkWithTabelltrekkByTrekkodeMetrics(tx)
+            val numberOfTabelltrekkByTrekkode: Map<Trekkode, Int> = SkattekortRepository.numberOfForskuddstrekkWithTabelltrekkByTrekkodeMetrics(tx)
             numberOfTabelltrekkByTrekkode.map { (trekkode, count) ->
                 numberOfTabelltrekkByTrekkodeMetric.labelValues(trekkode.value).set(count.toDouble())
             }
 //            Skattekort med tilleggsopplysning kildeskattpensjonist
 //            Skattekort med tilleggsopplysning opphold på Svalbard
-            val skattekortMedTilleggsopplysning: Map<Tilleggsopplysning, Int> = skattekortRepository.numberOfSkattekortByTilleggsopplysningMetrics(tx)
+            val skattekortMedTilleggsopplysning: Map<Tilleggsopplysning, Int> = SkattekortRepository.numberOfSkattekortByTilleggsopplysningMetrics(tx)
             skattekortMedTilleggsopplysning.map { (tilleggsopplysning, count) ->
                 skattekortMedTilleggsopplysningMetric.labelValues(tilleggsopplysning.value).set(count.toDouble())
             }
 //            Skattekort - frikort med beløpsgrense
 //            Skattekort - frikort uten beløpsgrense
-            val frikortByBeloepsGrenseJaNei: Map<String, Int> = skattekortRepository.numberOfFrikortMedUtenBeloepsgrense(tx)
+            val frikortByBeloepsGrenseJaNei: Map<String, Int> = SkattekortRepository.numberOfFrikortMedUtenBeloepsgrense(tx)
             frikortByBeloepsGrenseJaNei.map { (type, count) ->
                 frikortByBeloepsGrenseJaNeiMetric.labelValues(type).set(count.toDouble())
             }

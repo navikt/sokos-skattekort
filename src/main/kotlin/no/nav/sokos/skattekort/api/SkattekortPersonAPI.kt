20376ad4c42f05f438163399b16655b88a4b8b60
geirh-nav <geir.harris.hedemark@nav.no>
2025-11-22 11:58:42 +0100

    TOB-5661: Legg på autorisering og auditlogging, som i sokos-skattekort-person

--- src/main/kotlin/no/nav/sokos/skattekort/api/SkattekortPersonAPI.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/api/SkattekortPersonAPI.kt
@@ -1,5 +1,7 @@
 package no.nav.sokos.skattekort.api
 
+import com.auth0.jwt.JWT
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
@@ -7,15 +9,34 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 
 import no.nav.sokos.skattekort.api.skattekortpersonapi.v1.SkattekortPersonRequest
+import no.nav.sokos.skattekort.audit.Saksbehandler
 import no.nav.sokos.skattekort.module.skattekort.SkattekortPersonService
 
 fun Route.skattekortPersonApi(skattekortPersonService: SkattekortPersonService) {
     route("/api/v1") {
         post("hent-skattekort") {
             val skattekortPersonRequest: SkattekortPersonRequest = call.receive()
+            val saksbehandler = getSaksbehandler(call)
             call.respond(
-                skattekortPersonService.hentSkattekortPerson(skattekortPersonRequest),
+                skattekortPersonService.hentSkattekortPerson(skattekortPersonRequest, saksbehandler),
             )
         }
     }
+}
+
+private const val JWT_CLAIM_NAVIDENT = "NAVident"
+
+fun getSaksbehandler(call: ApplicationCall): Saksbehandler {
+    val oboToken =
+        call.request.headers["Authorization"]?.removePrefix("Bearer ")
+            ?: throw IllegalStateException("Could not get token from request header")
+    val navIdent = getNAVIdentFromToken(oboToken)
+
+    return Saksbehandler(navIdent)
+}
+
+private fun getNAVIdentFromToken(token: String): String {
+    val decodedJWT = JWT.decode(token)
+    return decodedJWT.claims[JWT_CLAIM_NAVIDENT]?.asString()
+        ?: throw RuntimeException("Missing NAVident in private claims")
 }

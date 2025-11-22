94cae92a55438bfcfac8ed10c6bac4a9f250c59f
geirh-nav <geir.harris.hedemark@nav.no>
2025-11-22 16:28:04 +0100

    TOB-5749: Fjern routingconfig også

--- src/main/kotlin/no/nav/sokos/skattekort/config/RoutingConfig.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/config/RoutingConfig.kt
@@ -6,13 +6,11 @@ import io.ktor.server.plugins.di.dependencies
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.routing
 
-import no.nav.sokos.skattekort.api.maskinportenTokenApi
 import no.nav.sokos.skattekort.api.skattekortApi
 import no.nav.sokos.skattekort.api.skattekortPersonApi
 import no.nav.sokos.skattekort.api.swaggerApi
 import no.nav.sokos.skattekort.module.forespoersel.ForespoerselService
 import no.nav.sokos.skattekort.module.skattekort.SkattekortPersonService
-import no.nav.sokos.skattekort.security.MaskinportenTokenClient
 
 fun Application.routingConfig(
     useAuthentication: Boolean,
@@ -22,11 +20,9 @@ fun Application.routingConfig(
         internalNaisRoutes(applicationState)
         swaggerApi()
         authenticate(useAuthentication, AUTHENTICATION_NAME) {
-            val maskinportenTokenClient: MaskinportenTokenClient by dependencies
             val forespoerselService: ForespoerselService by dependencies
             val skattekortPersonService: SkattekortPersonService by dependencies
 
-            maskinportenTokenApi(maskinportenTokenClient)
             skattekortApi(forespoerselService)
             skattekortPersonApi(skattekortPersonService)
         }

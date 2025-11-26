b3a5eaaee0ed4dc960b834c7233e6098d9adf629
geirh-nav <geir.harris.hedemark@nav.no>
2025-11-26 10:57:09 +0100

    TOB-5771: gjør abonnement nullable i utsending

--- src/main/resources/db/migration/V2025_11_26_17__aboid_nullable_i_utsending.sql
+++ src/main/resources/db/migration/V2025_11_26_17__aboid_nullable_i_utsending.sql
@@ -2,3 +2,4 @@ set lock_timeout = '1s';
 set statement_timeout = '5s';
 
 ALTER TABLE utsendinger ALTER COLUMN abonnement_id DROP NOT NULL;
+ALTER TABLE utsendinger ALTER COLUMN abonnement_id SET DEFAULT null;

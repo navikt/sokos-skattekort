82b6db27dee474a5479ffa8166c62594435645ae
geirh-nav <geir.harris.hedemark@nav.no>
2025-11-26 12:44:20 +0100

    TOB-5771: dropp abonnement_id i utsendinger

--- src/test/resources/database/utsending/skattekort_oppdragz.sql
+++ src/test/resources/database/utsending/skattekort_oppdragz.sql
@@ -4,6 +4,6 @@ VALUES (4001, '', 'OS');
 INSERT INTO abonnementer (id, forespoersel_id, person_id, inntektsaar)
 VALUES (5001, 4001, 3, 2025);
 
-INSERT INTO utsendinger (abonnement_id, fnr, inntektsaar, forsystem)
-VALUES (5001, '12345678903', 2025, 'OS');
+INSERT INTO utsendinger (fnr, inntektsaar, forsystem)
+VALUES ('12345678903', 2025, 'OS');
 

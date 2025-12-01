ed7c253672b65f7f6a2cbd5c62a9aaa332efb2d4
geirh-nav <geir.harris.hedemark@nav.no>
2025-12-01 13:39:07 +0100

    TOB-5661: Test for faktisk innlogging med oauth-server

--- src/test/kotlin/no/nav/sokos/skattekort/module/skattekortpersonapi/v1/SkattekortpersonApiE2ETest.kt
+++ src/test/kotlin/no/nav/sokos/skattekort/module/skattekortpersonapi/v1/SkattekortpersonApiE2ETest.kt
@@ -6,6 +6,7 @@ import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.read.ListAppender
 import com.atlassian.oai.validator.restassured.OpenApiValidationFilter
+import com.nimbusds.jwt.SignedJWT
 import io.kotest.assertions.json.shouldEqualJson
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.time.withConstantNow
@@ -13,38 +14,45 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldMatch
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
-import io.mockk.mockk
 import io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON
 import io.restassured.RestAssured
 import io.restassured.response.Response
+import io.restassured.specification.RequestSpecification
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.containsString
 import org.slf4j.LoggerFactory
 
-import no.nav.sokos.skattekort.TestUtil.readFile
 import no.nav.sokos.skattekort.infrastructure.DbListener
 import no.nav.sokos.skattekort.infrastructure.FullNettyApplication
 import no.nav.sokos.skattekort.infrastructure.MQListener
-import no.nav.sokos.skattekort.module.forespoersel.ForespoerselService
-
-private val forespoerselService = mockk<ForespoerselService>()
+import no.nav.sokos.skattekort.security.JWT_CLAIM_NAVIDENT
 
 class SkattekortpersonApiE2ETest :
     FunSpec({
         beforeTest {
-            FullNettyApplication.start()
+            try {
+                FullNettyApplication.start()
+            } catch (t: Throwable) {
+                println("Ouchie! " + t)
+            }
         }
 
         extensions(DbListener, MQListener)
 
-        val tokenWithNavIdent = readFile("/tokenWithNavIdent.txt")
+        val tokenWithNavIdent: SignedJWT =
+            FullNettyApplication.oauthServer.issueToken(
+                issuerId = "default",
+                claims =
+                    mapOf(JWT_CLAIM_NAVIDENT to "aUser"),
+            )
         val openApiValidationFilter = OpenApiValidationFilter("openapi/sokos-skattekort-person-v1-swagger.yaml")
-        val client =
+
+        fun client(): RequestSpecification =
             RestAssured
                 .given()
                 .filter(openApiValidationFilter)
                 .header(HttpHeaders.ContentType, APPLICATION_JSON.toString())
-                .header(HttpHeaders.Authorization, "Bearer $tokenWithNavIdent")
+                .header(HttpHeaders.Authorization, "Bearer ${tokenWithNavIdent.serialize()}")
                 .port(FullNettyApplication.PORT)
 
         test("for kort fnr dør på seg") {
@@ -52,7 +60,7 @@ class SkattekortpersonApiE2ETest :
                 DbListener.loadDataSet("database/skattekort/person_med_skattekort.sql")
 
                 val response: Response =
-                    client
+                    client()
                         .body(
                             """{
                         | "fnr": "1",
@@ -74,7 +82,7 @@ class SkattekortpersonApiE2ETest :
                 DbListener.loadDataSet("database/skattekort/person_med_skattekort.sql")
 
                 val response: Response =
-                    client
+                    client()
                         .body(
                             """{
                             | "fnr": "a2345678901",
@@ -96,7 +104,7 @@ class SkattekortpersonApiE2ETest :
                 DbListener.loadDataSet("database/skattekort/person_med_skattekort.sql")
 
                 val response: Response =
-                    client
+                    client()
                         .body(
                             """{
                             | "fnr": "12345678901",
@@ -112,7 +120,7 @@ class SkattekortpersonApiE2ETest :
                 assertThat("Vi får en sane feilmelding", response.body().prettyPrint(), containsString("Gyldig årstall er mellom 2024 og inneværende år, var 20252"))
             }
         }
-        test("vi kan hente et skattekort") {
+        test("vi kan hente et prosent-skattekort") {
             withConstantNow(LocalDateTime.parse("2025-04-12T00:00:00")) {
                 DbListener.loadDataSet("database/skattekort/person_med_skattekort.sql")
                 val listAppender = ListAppender<ILoggingEvent>()
@@ -123,7 +131,7 @@ class SkattekortpersonApiE2ETest :
                 auditLogger.addAppender(listAppender)
                 try {
                     val response: Response =
-                        client
+                        client()
                             .body(
                                 """{
                             | "fnr": "12345678901",
@@ -161,11 +169,52 @@ class SkattekortpersonApiE2ETest :
                     )
                     listAppender.list.size shouldBe 1
                     listAppender.list.get(0).formattedMessage shouldMatch
-                        "CEF\\:0\\|Utbetalingsportalen\\|sokos\\-skattekort\\|1\\.0\\|audit\\:access\\|sokos\\-skattekort\\|INFO\\|suid\\=Z123456 duid\\=12345678901 end=\\d+ msg\\=NAV\\-ansatt har søkt etter skattekort for bruker"
+                        "CEF\\:0\\|Utbetalingsportalen\\|sokos\\-skattekort\\|1\\.0\\|audit\\:access\\|sokos\\-skattekort\\|INFO\\|suid\\=aUser duid\\=12345678901 end=\\d+ msg\\=NAV\\-ansatt har søkt etter skattekort for bruker"
                 } finally {
                     auditLogger.detachAppender(listAppender)
                     listAppender.stop()
                 }
+            }
+        }
+        test("vi kan hente et frikort med beløpsgrense") {
+            withConstantNow(LocalDateTime.parse("2025-04-12T00:00:00")) {
+                DbListener.loadDataSet("database/skattekort/person_med_skattekort.sql")
+
+                val response: Response =
+                    client()
+                        .body(
+                            """{
+                            | "fnr": "12345678902",
+                            | "inntektsaar": 2025
+                            | }
+                            """.trimMargin(),
+                        ).post("/api/v1/hent-skattekort")
+                        .then()
+                        .assertThat()
+                        .statusCode(HttpStatusCode.OK.value)
+                        .extract()
+                        .response()!!
+                response.body().prettyPrint().shouldEqualJson(
+                    """[
+  {
+    "inntektsaar": 2025,
+    "arbeidstakeridentifikator": "12345678902",
+    "resultatPaaForespoersel": "skattekortopplysningerOK",
+    "skattekort": {
+      "utstedtDato": "2025-11-11",
+      "skattekortidentifikator": 18,
+      "forskuddstrekk": [
+        {
+          "type": "Frikort",
+          "trekkode": "LOENN_FRA_NAV",
+          "frikortbeloep": 65000
+        }
+      ]
+    },
+    "tilleggsopplysning": []
+  }
+]""",
+                )
             }
         }
     })

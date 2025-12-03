4ac7bf576cf7dff8e4335c27da929d0e474b6b75
takwaiwang <tak.wai.wang@nav.no>
2025-12-03 09:20:07 +0100

    TOB-5115 endre httpclient fra apache til CIO

--- src/main/kotlin/no/nav/sokos/skattekort/config/HttpClientConfig.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/config/HttpClientConfig.kt
@@ -1,21 +1,18 @@
 package no.nav.sokos.skattekort.config
 
-import java.net.ProxySelector
-
 import kotlinx.serialization.json.Json
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import mu.KotlinLogging
-import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 
 private val logger = KotlinLogging.logger {}
 
 fun createHttpClient(): HttpClient =
-    HttpClient(Apache) {
+    HttpClient(CIO) {
         expectSuccess = false
 
         install(ContentNegotiation) {
@@ -35,11 +32,5 @@ fun createHttpClient(): HttpClient =
                 logger.warn { "$retryCount retry feilet mot: ${request.url}" }
             }
             exponentialDelay()
-        }
-
-        engine {
-            customizeClient {
-                setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault()))
-            }
         }
     }

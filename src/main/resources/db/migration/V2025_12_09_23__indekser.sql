69f71a6dd886d1bc151bddd56c1baefa2d036239
geirh-nav <geir.harris.hedemark@nav.no>
2025-12-09 13:05:35 +0100

    TOB-5716: øk timeouts

--- src/main/resources/db/migration/V2025_12_09_23__indekser.sql
+++ src/main/resources/db/migration/V2025_12_09_23__indekser.sql
@@ -1,14 +1,14 @@
-set lock_timeout = '1s';
-set statement_timeout = '5s';
+set lock_timeout = '10s';
+set statement_timeout = '10s';
 
-CREATE INDEX IF NOT EXISTS bestillinger_inntektsaar ON bestillinger(inntektsaar);
-CREATE INDEX IF NOT EXISTS bestillinger_oppdatert ON bestillinger(oppdatert);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS bestillinger_inntektsaar ON bestillinger(inntektsaar);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS bestillinger_oppdatert ON bestillinger(oppdatert);
 
-CREATE INDEX IF NOT EXISTS skattekort_inntektsaar ON skattekort(inntektsaar);
-CREATE INDEX IF NOT EXISTS skattekort_opprettet ON skattekort(opprettet);
-CREATE INDEX IF NOT EXISTS skattekort_resultat ON skattekort(resultatforskattekort);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS skattekort_inntektsaar ON skattekort(inntektsaar);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS skattekort_opprettet ON skattekort(opprettet);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS skattekort_resultat ON skattekort(resultatforskattekort);
 
-CREATE INDEX IF NOT EXISTS forskuddstrekk_trek_kode ON forskuddstrekk(trekk_kode);
-CREATE INDEX IF NOT EXISTS forskuddstrekk_type ON forskuddstrekk(type);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS forskuddstrekk_trek_kode ON forskuddstrekk(trekk_kode);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS forskuddstrekk_type ON forskuddstrekk(type);
 
-CREATE INDEX IF NOT EXISTS skattekort_tilleggsopplysning_opplysning ON skattekort_tilleggsopplysning(opplysning);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS skattekort_tilleggsopplysning_opplysning ON skattekort_tilleggsopplysning(opplysning);

7fbc95c4f0ba69cce480ad06b8a74c00e28bd106
geirh-nav <geir.harris.hedemark@nav.no>
2025-12-12 11:44:21 +0100

    TOB-5928: Squawk bra om forbedringer, manglende throw

--- src/main/resources/db/migration/V2025_12_12_37__beviskolonne_mottatte_skattekort.sql
+++ src/main/resources/db/migration/V2025_12_12_37__beviskolonne_mottatte_skattekort.sql
@@ -2,4 +2,3 @@ set lock_timeout = '10s';
 set statement_timeout = '10s';
 
 ALTER TABLE bestillingsbatcher ADD COLUMN IF NOT EXISTS data_mottatt TEXT NULL DEFAULT NULL;
-CREATE INDEX CONCURRENTLY IF NOT EXISTS bestillingsbatcher_bestillingsreferanse ON bestillingsbatcher (bestillingsreferanse);

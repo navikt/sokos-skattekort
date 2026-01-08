9f237ff85c480428a1a700641e0f6fb138c8f779
Lars Hartvigsen <lars.hartvigsen@nav.no>
2026-01-08 09:16:48 +0100

    Glemte en .inc

--- src/main/kotlin/no/nav/sokos/skattekort/skatteetaten/SkatteetatenClient.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/skatteetaten/SkatteetatenClient.kt
@@ -61,7 +61,7 @@ class SkatteetatenClient(
                 accept(ContentType.Application.Json)
                 expectSuccess = false
             }
-        hentBestillingReturkode.labelValues(response.status.description)
+        hentBestillingReturkode.labelValues(response.status.description).inc()
 
         if (response.status == HttpStatusCode.NoContent) {
             hentBestillingFeilet.labelValues(bestillingsreferanse).inc()

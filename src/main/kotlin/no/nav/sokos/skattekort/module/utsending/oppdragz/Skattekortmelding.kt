ad95177942b4dfb658c6fb6887f9fc104c622853
geirh-nav <geir.harris.hedemark@nav.no>
2026-01-27 12:23:04 +0100

    TOB-5685: Fjern forskuddstrekk-klassen fra os-eskatt

--- src/main/kotlin/no/nav/sokos/skattekort/module/utsending/oppdragz/Skattekortmelding.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/module/utsending/oppdragz/Skattekortmelding.kt
@@ -1,13 +1,9 @@
 package no.nav.sokos.skattekort.module.utsending.oppdragz
 
-import java.math.BigDecimal
 import javax.xml.datatype.DatatypeFactory
 
-import no.nav.sokos.skattekort.module.skattekort.Prosentkort
 import no.nav.sokos.skattekort.module.skattekort.ResultatForSkattekort
-import no.nav.sokos.skattekort.module.skattekort.Tabellkort
 import no.nav.sokos.skattekort.module.skattekort.Tilleggsopplysning
-import no.nav.sokos.skattekort.module.skattekort.Trekkode
 
 data class Skattekortmelding(
     val inntektsaar: Long = 0,
@@ -25,14 +21,7 @@ data class Skattekortmelding(
                 inntektsaar = sk.inntektsaar.toLong(),
                 utstedtDato = sk.utstedtDato?.toString()?.let { DatatypeFactory.newInstance().newXMLGregorianCalendar(it) },
                 skattekortidentifikator = sk.identifikator?.toLong(),
-                forskuddstrekk =
-                    sk.forskuddstrekkList.map {
-                        when (it) {
-                            is no.nav.sokos.skattekort.module.skattekort.Frikort -> Frikort(Trekkode.fromValue(it.trekkode.value), it.frikortBeloep?.let { belop -> BigDecimal(belop) })
-                            is Prosentkort -> Trekkprosent(Trekkode.fromValue(it.trekkode.value), it.prosentSats, it.antallMndForTrekk)
-                            is Tabellkort -> Trekktabell(Trekkode.fromValue(it.trekkode.value), Tabelltype.TREKKTABELL_FOR_LOENN, it.tabellNummer, it.prosentSats, it.antallMndForTrekk)
-                        }
-                    },
+                forskuddstrekk = sk.forskuddstrekkList,
             ),
         tilleggsopplysning =
             sk.tilleggsopplysningList,

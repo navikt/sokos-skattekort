78c470cd6d09c0c94dc865506f84f1d2b92a145c
takwaiwang <tak.wai.wang@nav.no>
2026-02-03 16:02:08 +0100

    Fjern byte-buddy avhengighet siden mockk har bumped med oppdatering

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,7 +133,6 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-json:$kotestVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
-    testImplementation("net.bytebuddy:byte-buddy:1.18.4") // TEMP: Needed for mockk 1.14.6 with java25. Remove when Mockk is updated and bytebuddy is no longer needed.
     testImplementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
     testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")

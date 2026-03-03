777cace1596d6c7fe6a3324235c4a5f9e110c52d
Lars Hartvigsen <lars.hartvigsen@nav.no>
2026-03-03 13:38:01 +0100

    Ny versjon av api for å ta med mer informasjon på dto

---
+++ src/main/kotlin/no/nav/sokos/skattekort/dto/v2/SkattekortDTO.kt
@@ -0,0 +1,167 @@
+package no.nav.sokos.skattekort.dto.v2
+
+import java.math.BigDecimal
+
+import kotlin.time.Instant
+import kotlinx.datetime.LocalDate
+import kotlinx.serialization.Serializable
+
+import no.nav.sokos.skattekort.module.person.PersonId
+import no.nav.sokos.skattekort.module.skattekort.Forskuddstrekk
+import no.nav.sokos.skattekort.module.skattekort.Frikort
+import no.nav.sokos.skattekort.module.skattekort.Prosentkort
+import no.nav.sokos.skattekort.module.skattekort.ResultatForSkattekort
+import no.nav.sokos.skattekort.module.skattekort.Skattekort
+import no.nav.sokos.skattekort.module.skattekort.SkattekortKilde
+import no.nav.sokos.skattekort.module.skattekort.Tabellkort
+import no.nav.sokos.skattekort.module.skattekort.Tilleggsopplysning
+import no.nav.sokos.skattekort.module.skattekort.Trekkode
+
+@Serializable
+data class SkattekortDTO(
+    val forskuddstrekkList: List<ForskuddstrekkDTO>,
+    val id: Long,
+    val identifikator: String?,
+    val inntektsaar: Int,
+    val kilde: SkattekortKilde,
+    val opprettet: Instant,
+    val resultatForSkattekort: String?,
+    val tilleggsopplysningList: List<String> = emptyList(),
+    val utstedtDato: LocalDate?,
+) {
+    constructor(skattekort: Skattekort) : this(
+        forskuddstrekkList =
+            skattekort.forskuddstrekkList.map {
+                when (it) {
+                    is Frikort -> {
+                        ForskuddstrekkDTO(
+                            trekkode = it.trekkode.value,
+                            frikort = FrikortDTO(frikortBeloep = it.frikortBeloep),
+                        )
+                    }
+
+                    is Prosentkort -> {
+                        ForskuddstrekkDTO(
+                            trekkode = it.trekkode.value,
+                            prosentkort =
+                                ProsentkortDTO(
+                                    prosentSats = it.prosentSats.toDouble(),
+                                    antallMndForTrekk = it.antallMndForTrekk?.toDouble(),
+                                ),
+                        )
+                    }
+
+                    is Tabellkort -> {
+                        ForskuddstrekkDTO(
+                            trekkode = it.trekkode.value,
+                            trekktabell =
+                                TabellkortDTO(
+                                    tabell = it.tabellNummer,
+                                    prosentSats = it.prosentSats.toDouble(),
+                                    antallMndForTrekk = it.antallMndForTrekk.toDouble(),
+                                ),
+                        )
+                    }
+                }
+            },
+        id = skattekort.id?.value ?: throw IllegalStateException("Skattekort fra databasen mangler id"),
+        identifikator = skattekort.identifikator,
+        inntektsaar = skattekort.inntektsaar,
+        kilde = SkattekortKilde.valueOf(skattekort.kilde),
+        opprettet = skattekort.opprettet,
+        resultatForSkattekort = skattekort.resultatForSkattekort.value,
+        tilleggsopplysningList = skattekort.tilleggsopplysningList.map { it.value },
+        utstedtDato = skattekort.utstedtDato,
+    )
+
+    fun toDomainSkattekort(
+        personId: PersonId,
+        utstedtDato: LocalDate,
+        identifikator: String?,
+        kilde: SkattekortKilde,
+    ): Skattekort =
+        Skattekort(
+            personId = personId,
+            utstedtDato = utstedtDato,
+            identifikator = identifikator,
+            kilde = kilde.value,
+            inntektsaar = this.inntektsaar,
+            resultatForSkattekort = resultatForSkattekort?.let(ResultatForSkattekort::fromValue) ?: ResultatForSkattekort.SkattekortopplysningerOK,
+            forskuddstrekkList = this.forskuddstrekkList.map { it.toDomainForskuddstrekk() },
+            tilleggsopplysningList = this.tilleggsopplysningList.map { Tilleggsopplysning.fromValue(it) },
+        )
+}
+
+@Serializable
+data class ForskuddstrekkDTO(
+    val trekkode: String,
+    val frikort: FrikortDTO? = null,
+    val prosentkort: ProsentkortDTO? = null,
+    val trekktabell: TabellkortDTO? = null,
+) {
+    fun toDomainForskuddstrekk(): Forskuddstrekk =
+        when {
+            frikort != null -> {
+                Frikort(
+                    trekkode = Trekkode.fromValue(trekkode),
+                    frikortBeloep = frikort.frikortBeloep,
+                )
+            }
+
+            trekktabell != null -> {
+                Tabellkort(
+                    trekkode = Trekkode.fromValue(trekkode),
+                    tabellNummer = trekktabell.tabell,
+                    prosentSats = BigDecimal.valueOf(trekktabell.prosentSats),
+                    antallMndForTrekk = BigDecimal.valueOf(trekktabell.antallMndForTrekk),
+                )
+            }
+
+            prosentkort != null -> {
+                Prosentkort(
+                    trekkode = Trekkode.fromValue(trekkode),
+                    prosentSats = BigDecimal.valueOf(prosentkort.prosentSats),
+                    antallMndForTrekk =
+                        prosentkort.antallMndForTrekk?.let { antallMnd ->
+                            BigDecimal.valueOf(antallMnd)
+                        },
+                )
+            }
+
+            else -> {
+                error("Ugyldig Forskuddstrekk: $this")
+            }
+        }
+}
+
+@Serializable
+data class FrikortDTO(
+    val frikortBeloep: Int? = null,
+)
+
+@Serializable
+data class ProsentkortDTO(
+    val prosentSats: Double,
+    val antallMndForTrekk: Double? = null,
+)
+
+@Serializable
+data class TabellkortDTO(
+    val tabell: String,
+    val prosentSats: Double,
+    val antallMndForTrekk: Double,
+)
+
+val validTrekkodeList =
+    listOf(
+        Trekkode.LOENN_FRA_NAV,
+        Trekkode.PENSJON_FRA_NAV,
+        Trekkode.UFOERETRYGD_FRA_NAV,
+    )
+
+val validTilleggsopplysningList =
+    listOf(
+        Tilleggsopplysning.OPPHOLD_PAA_SVALBARD.value,
+        Tilleggsopplysning.KILDESKATT_PAA_PENSJON.value,
+        Tilleggsopplysning.OPPHOLD_I_TILTAKSSONE.value,
+    )

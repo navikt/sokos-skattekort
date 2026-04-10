eee0aa676bd722b48bb08361d794b407e06e5b98
Lars Hartvigsen <lars.hartvigsen@nav.no>
2026-04-10 09:05:33 +0200

    TOB-5267 Endepunkt for å hente ut auditlogg

--- src/main/kotlin/no/nav/sokos/skattekort/api/SkattekortAdminApi.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/api/SkattekortAdminApi.kt
@@ -7,25 +7,37 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import mu.KotlinLogging
 
+import no.nav.sokos.skattekort.api.model.AuditResponse
 import no.nav.sokos.skattekort.api.model.BatchInsightRequest
 import no.nav.sokos.skattekort.api.model.BatchInsightResponse
+import no.nav.sokos.skattekort.api.model.FnrRequest
+import no.nav.sokos.skattekort.person.PersonService
 import no.nav.sokos.skattekort.skattekortbestilling.BestillingsbatchService
 
 private val logger = KotlinLogging.logger { }
 
 const val BASE_PATH_ADMIN = "/api/v1/admin"
 
-fun Route.skattekortAdminApi(bestillingsbatchService: BestillingsbatchService) {
+fun Route.skattekortAdminApi(
+    bestillingsbatchService: BestillingsbatchService,
+    personService: PersonService,
+) {
     route(BASE_PATH_ADMIN) {
         post("hentBatcher") {
             // Utkommentert fordi vi ikke har deployet ennå
 //            call.requireScope(requiredScope = Scope.ADMIN_SCOPE)
             val request = call.receive<BatchInsightRequest>()
-            logger.info("Fra: ${request.tidspunktFom}, Til: ${request.tidspunktTom}")
             val bestillingsbatcher = bestillingsbatchService.getBestillingsbatches(request.tidspunktFom, request.tidspunktTom)
             call.respond(
                 BatchInsightResponse(bestillingsbatcher),
             )
+        }
+        post("auditLogg") {
+            // Utkommentert fordi vi ikke har deployet ennå
+//            call.requireScope(requiredScope = Scope.ADMIN_SCOPE)
+            val request = call.receive<FnrRequest>()
+            val audits = personService.getAuditLogs(request.fnr)
+            call.respond(AuditResponse(audits))
         }
     }
 }

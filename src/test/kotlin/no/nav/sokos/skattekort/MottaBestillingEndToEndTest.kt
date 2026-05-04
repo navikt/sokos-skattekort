a8eb702f70e43a8c036765a25a4d28bc7b0d326a
Thea Marie Alnes <thea.marie.alnes@nav.no>
2026-05-04 15:11:39 +0200

    Kommenter ut E2E-tester som bruker withFullTestApplication
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- src/test/kotlin/no/nav/sokos/skattekort/MottaBestillingEndToEndTest.kt
+++ src/test/kotlin/no/nav/sokos/skattekort/MottaBestillingEndToEndTest.kt
@@ -1,71 +1,56 @@
 package no.nav.sokos.skattekort
 
-import java.time.LocalDateTime
-
-import io.kotest.assertions.assertSoftly
-import io.kotest.assertions.nondeterministic.eventually
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.extensions.time.withConstantNow
-import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.shouldBe
-
-import no.nav.sokos.skattekort.forespoersel.AbonnementRepository
-import no.nav.sokos.skattekort.forespoersel.ForespoerselRepository
-import no.nav.sokos.skattekort.forespoersel.Forsystem
-import no.nav.sokos.skattekort.listener.DbListener
-import no.nav.sokos.skattekort.listener.MQListener
-import no.nav.sokos.skattekort.listener.MQListener.forespoerselQueue
-import no.nav.sokos.skattekort.listener.WiremockListener
-import no.nav.sokos.skattekort.util.SQLUtils.transaction
-import no.nav.sokos.skattekort.utils.TestUtils.eventuallyConfiguration
-import no.nav.sokos.skattekort.utils.TestUtils.withFullTestApplication
-import no.nav.sokos.skattekort.utsending.UtsendingRepository
 
 class MottaBestillingEndToEndTest :
     FunSpec({
-        extensions(DbListener, MQListener, WiremockListener)
+        // TODO: Disse E2E-testene bruker withFullTestApplication og skal skrives om.
+        //  Unik dekning: JMS-melding → listener → ForespoerselService → DB (hele flyten).
+        //  ForespoerselServiceTest dekker service-laget, men ikke MQ-listener-integrasjonen.
 
-        test("vi kan håndtere en forespørsel fra OS") {
-            withConstantNow(LocalDateTime.parse("2025-04-12T00:00:00")) {
-                withFullTestApplication {
-                    // Last inn SQL testdata
-                    DbListener.loadDataSet("basicendtoendtest/basicdata.sql")
-
-                    val fnr = "15467834260"
-                    WiremockListener.wiremockPDLStub(WiremockListener.generateHentIdenterBolk(fnr))
-                    JmsTestUtil.sendMessage(msg = "OS;2027;$fnr", queue = forespoerselQueue)
-
-                    eventually(eventuallyConfiguration) {
-                        DbListener.dataSource.transaction { tx ->
-                            val forespoerselList = ForespoerselRepository.getAllForespoersel(tx)
-
-                            forespoerselList shouldHaveSize 1
-                            assertSoftly {
-                                forespoerselList.first().forsystem shouldBe Forsystem.OPPDRAGSSYSTEMET
-                                forespoerselList.first().dataMottatt shouldBe "OS;2027;$fnr"
-                            }
-
-                            val abonnementList = AbonnementRepository.getAllAbonnementer(tx)
-
-                            abonnementList shouldHaveSize 1
-                            assertSoftly("Det skal ha blitt opprettet et abonnement") {
-                                abonnementList
-                                    .first()
-                                    .person.foedselsnummer.fnr.value shouldBe fnr
-                                abonnementList.first().inntektsaar shouldBe 2027
-                                abonnementList.first().forespoersel.forsystem shouldBe Forsystem.OPPDRAGSSYSTEMET
-                                abonnementList.first().inntektsaar shouldBe 2027
-                            }
-
-                            val utsendingList = UtsendingRepository.getAllUtsendinger(tx)
-
-                            assertSoftly("Det skal ikke ha blitt opprettet en utsending") {
-                                utsendingList shouldHaveSize 0
-                            }
-                        }
-                    }
-                    JmsTestUtil.assertQueueIsEmpty(forespoerselQueue)
-                }
-            }
-        }
+        // extensions(DbListener, MQListener, WiremockListener)
+        //
+        // test("vi kan håndtere en forespørsel fra OS") {
+        //     withConstantNow(LocalDateTime.parse("2025-04-12T00:00:00")) {
+        //         withFullTestApplication {
+        //             // Last inn SQL testdata
+        //             DbListener.loadDataSet("basicendtoendtest/basicdata.sql")
+        //
+        //             val fnr = "15467834260"
+        //             WiremockListener.wiremockPDLStub(WiremockListener.generateHentIdenterBolk(fnr))
+        //             JmsTestUtil.sendMessage(msg = "OS;2027;$fnr", queue = forespoerselQueue)
+        //
+        //             eventually(eventuallyConfiguration) {
+        //                 DbListener.dataSource.transaction { tx ->
+        //                     val forespoerselList = ForespoerselRepository.getAllForespoersel(tx)
+        //
+        //                     forespoerselList shouldHaveSize 1
+        //                     assertSoftly {
+        //                         forespoerselList.first().forsystem shouldBe Forsystem.OPPDRAGSSYSTEMET
+        //                         forespoerselList.first().dataMottatt shouldBe "OS;2027;$fnr"
+        //                     }
+        //
+        //                     val abonnementList = AbonnementRepository.getAllAbonnementer(tx)
+        //
+        //                     abonnementList shouldHaveSize 1
+        //                     assertSoftly("Det skal ha blitt opprettet et abonnement") {
+        //                         abonnementList
+        //                             .first()
+        //                             .person.foedselsnummer.fnr.value shouldBe fnr
+        //                         abonnementList.first().inntektsaar shouldBe 2027
+        //                         abonnementList.first().forespoersel.forsystem shouldBe Forsystem.OPPDRAGSSYSTEMET
+        //                         abonnementList.first().inntektsaar shouldBe 2027
+        //                     }
+        //
+        //                     val utsendingList = UtsendingRepository.getAllUtsendinger(tx)
+        //
+        //                     assertSoftly("Det skal ikke ha blitt opprettet en utsending") {
+        //                         utsendingList shouldHaveSize 0
+        //                     }
+        //                 }
+        //             }
+        //             JmsTestUtil.assertQueueIsEmpty(forespoerselQueue)
+        //         }
+        //     }
+        // }
     })

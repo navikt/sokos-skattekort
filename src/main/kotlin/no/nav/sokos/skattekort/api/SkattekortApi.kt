4f05c7b618ba358100ac749bfcbbc6743322e6d7
Lars Hartvigsen <lars.hartvigsen@nav.no>
2026-05-15 14:36:27 +0200

    TOB-6603 SKJERMET som mulig status

--- src/main/kotlin/no/nav/sokos/skattekort/api/SkattekortApi.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/api/SkattekortApi.kt
@@ -45,13 +45,14 @@ fun Route.skattekortApi(
         post("status") {
             call.requireScope(Scope.STATUS_SCOPE)
             val request = call.receive<ForespoerselRequest>()
-            val saksbehandler = call.getNavIdentOrNull()?.let { Saksbehandler(it) }
+            val saksbehandler = call.getNavIdentOrNull()?.let { Saksbehandler(it) } ?: return@post call.respond(HttpStatusCode.Unauthorized)
 
             logger.info(marker = TEAM_LOGS_MARKER) {
-                "skattekortApi - Mottatt forespørsel: $request på vegne av ${saksbehandler?.ident}"
+                "skattekortApi - Mottatt forespørsel: $request på vegne av ${saksbehandler.ident}"
             }
+
             call.respond(
-                StatusResponse(statusService.statusForespoeresel(request.personIdent, request.aar, request.forsystem)),
+                StatusResponse(statusService.statusForespoeresel(request.personIdent, request.aar, request.forsystem, saksbehandler)),
             )
         }
     }

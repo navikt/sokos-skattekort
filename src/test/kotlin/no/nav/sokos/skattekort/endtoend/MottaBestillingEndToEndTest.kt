15c800238a4dd8109cdceb251193596e03cb7143
Tak Wai Wang <takwaiwang@NO-KQ9QQ7P4MC.local>
2026-05-28 15:05:43 +0200

    Test

--- src/test/kotlin/no/nav/sokos/skattekort/endtoend/MottaBestillingEndToEndTest.kt
+++ src/test/kotlin/no/nav/sokos/skattekort/endtoend/MottaBestillingEndToEndTest.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.FunSpec
 
 class MottaBestillingEndToEndTest :
     FunSpec({
+
         // Dekningen fra denne filen er flyttet til:
         // - ForespoerselListenerTest: JMS-melding → listener → ForespoerselService → DB (MQ-listener-integrasjon)
         // - ForespoerselServiceTest: service-laget inkl. opprettelse av forespoersel, abonnement, bestilling og utsending

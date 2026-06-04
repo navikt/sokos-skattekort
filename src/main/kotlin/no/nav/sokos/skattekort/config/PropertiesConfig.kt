f37d82ced6fb4fdbaa402652466aab1a9edf1fea
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-06-04 09:14:14 +0200

    Gjør APPLICATION_ENV tilgjengelig fra System.getProperty også

--- src/main/kotlin/no/nav/sokos/skattekort/config/PropertiesConfig.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/config/PropertiesConfig.kt
@@ -199,6 +199,7 @@ fun ApplicationConfig.loadEnvironmentConfig(): ApplicationConfig {
     val hoconConfig = HoconApplicationConfig(ConfigFactory.load())
     val environmentName =
         System.getenv("APPLICATION_ENV")
+            ?: System.getProperty("APPLICATION_ENV")
             ?: System.getenv("NAIS_CLUSTER_NAME")
             ?: System.getProperty("NAIS_CLUSTER_NAME")
     val environment = environmentName?.lowercase()?.substringBefore("-") ?: "local"

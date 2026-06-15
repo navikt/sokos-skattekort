21146e6720fc642df633f25d46933e5fa2a585ad
takwaiwang <102529802+takwaiwang@users.noreply.github.com>
2026-06-15 15:00:03 +0200

    TOB-6614 reversere batch logikk

--- src/main/kotlin/no/nav/sokos/skattekort/config/JobTaskConfig.kt
+++ src/main/kotlin/no/nav/sokos/skattekort/config/JobTaskConfig.kt
@@ -12,7 +12,6 @@ import com.github.kagkarlsson.scheduler.task.helper.Tasks
 import com.github.kagkarlsson.scheduler.task.schedule.Schedules.cron
 import mu.KotlinLogging
 
-import no.nav.sokos.skattekort.forespoersel.ForespoerselService
 import no.nav.sokos.skattekort.infrastructure.MetricsService
 import no.nav.sokos.skattekort.skattekort.SkattekortService
 import no.nav.sokos.skattekort.skattekortbestilling.BestillingsbatchService
@@ -27,7 +26,6 @@ private const val JOB_TASK_SEND_BESTILLING_BATCH = "sendBestilling"
 private const val JOB_TASK_SEND_UTSENDING_BATCH = "sendUtsending"
 private const val JOB_TASK_HENT_OPPDATERTE_SKATTEKORT_BATCH = "hentOppdaterteSkattekort"
 private const val JOB_TASK_FETCH_METRICS = "fetchMetrics"
-private const val JOB_TASK_FORESPOERSEL_INPUT = "forespoerselInput"
 private const val JOB_TASK_DELETE_SKATTEKORT = "deleteSkattekort"
 
 object JobTaskConfig {
@@ -39,7 +37,6 @@ object JobTaskConfig {
         utsendingService: UtsendingService,
         skattekortdataService: SkattekortDataService,
         metricsService: MetricsService,
-        forespoerselService: ForespoerselService,
         skattekortService: SkattekortService,
         dataSource: DataSource,
     ): Scheduler =
